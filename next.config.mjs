9fa9b55ba097b9384c282c12cbe16f55b3cd05b9
Mateusz <echelon133@gmail.com>
2024-07-03 21:37:13 +0200

    Configure external image sources and declare urls of backend services

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    MATCHES_BASE_URL: `${process.env.BASE_URL}/api/matches`,
+    COMPETITIONS_BASE_URL: `${process.env.BASE_URL}/api/competitions`,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/wikipedia/**'
+      },
+    ]
+  }
 };
 
 export default nextConfig;
