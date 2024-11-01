09ff9c9718748574400da23ccbcc0cb41b23a154
Mateusz <echelon133@gmail.com>
2024-11-01 20:55:57 +0100

    Change `DatePicker`'s styling to make it narrower

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -74,13 +74,13 @@ export default function DatePicker(props: {
 
   return (
     <>
-      <div className="flex flex-row w-full h-14 bg-c2 items-center justify-center">
-        <div className="basis-1/6">
+      <div className="flex flex-row h-12 bg-c2 items-center justify-center">
+        <div className="basis-1/12">
           <button onClick={pickPreviousOption} className="bg-white flex rounded-lg float-right hover:bg-gray hover:bg-opacity-25 hover:text-gray">
             <Image width="30" height="30" src="chevron-left.svg" alt="Previous day" />
           </button>
         </div>
-        <div className="relative basis-4/6">
+        <div className="relative basis-4/12 mx-1">
           <button onClick={togglePickerListVisibility} className="bg-white text-black flex rounded-lg w-full items-center justify-center hover:bg-gray hover:bg-opacity-25 hover:text-white">
             <Image className="float-left" width="30" height="30" src="calendar.svg" alt="Precise date picker" />
             <span className="font-bold mt-1 pl-2">{pickerOptions.get(props.selectedDateKey)!.displayName}</span>
@@ -95,7 +95,7 @@ export default function DatePicker(props: {
             })}
           </ul>
         </div>
-        <div className="basis-1/6">
+        <div className="basis-1/12">
           <button onClick={pickNextOption} className="bg-white flex rounded-lg float-left hover:bg-gray hover:bg-opacity-25 hover:text-gray">
             <Image width="30" height="30" src="chevron-right.svg" alt="Next day" />
           </button>
