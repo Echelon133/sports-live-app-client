2e7c7a5845c8bc2c4d3902bbc7c09b1f649fe202
Mateusz <echelon133@gmail.com>
2024-12-28 00:03:26 +0100

    Extend tailwind colors instead of replacing them, and remove unnecessary custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,30 +13,24 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'c0': '#000814',
+        'c1': '#001d3d',
+        'c2': '#003566',
+        'c3': '#fde047',
+        'c4': '#ffd60a',
+        'positive-a': '#1d4ed8',
+        'positive-b': '#86198f',
+        'positive-c': '#059669',
+        'positive-d': '#a855f7',
+        'negative-a': '#881337',
+        'negative-b': '#f43f5e',
+        'highlight-a': '#491130',
+        'highlight-b': '#ff0000',
+      }
     },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'black': '#000000',
-      'gray': '#6c757d',
-      'green': '#4f772d',
-      'red': '#9a031e',
-      'yellow': '#ffd60a',
-      'c0': '#000814',
-      'c1': '#001d3d',
-      'c2': '#003566',
-      'c3': '#fde047',
-      'c4': '#ffd60a',
-      'positive-a': '#1d4ed8',
-      'positive-b': '#86198f',
-      'positive-c': '#059669',
-      'positive-d': '#a855f7',
-      'negative-a': '#881337',
-      'negative-b': '#f43f5e',
-      'highlight-a': '#491130',
-      'highlight-b': '#ff0000',
-    }
   },
   // all colors which are applied to elements dynamically should
   // be placed here so that they are always preloaded, otherwise
@@ -48,6 +42,12 @@ const config: Config = {
     'bg-positive-d',
     'bg-negative-a',
     'bg-negative-b',
+    // form square color when a team has won
+    'bg-green-700',
+    // form square color when a team has lost
+    'bg-red-600',
+    // form square color when a team has drawn
+    'bg-gray-600'
   ],
   plugins: [],
 };
